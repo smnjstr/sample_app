b834f7679f04e65f78fabd37ce36049a7a758170
Ubuntu <ubuntu@ip-172-31-45-115.ec2.internal>
2020-08-21 11:27:39 +0000

    Finish sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,13 @@ class User < ApplicationRecord
     before_save {self.email = self.email.downcase}
 #    <from exercise 6.35> before_save { email.downcase! }
     validates(:name,     { presence: true,
-                           length: { maximum: 50 }
-    })
+                           length: { maximum: 50 } } )
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates(:email,    { presence: true, 
                            length: { maximum: 255},
                            format: { with: VALID_EMAIL_REGEX},
-                           uniqueness: true
-    })
+                           uniqueness: true } )
     has_secure_password
-    validates(:password,  { presence: true, 
-                            length: { minimum: 6 }
-        
-    })
+    validates(:password, { presence: true, 
+                           length: { minimum: 6 } } )
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,43 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+# Test invalid signup fails safely
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do 
+      post users_path, params: { user: { name: "",
+                                         email: "user@invalid",
+                                         password: "foo",
+                                         password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'       # book solution
+    assert_select 'div.field_with_errors'       # book solution
+  end
+
+  test "error messages should be triggered" do
+    @user = User.create(name: "",
+                        email: "user@invalid",
+                        password: "foo",
+                        password_confirmation: "bar")
+    assert @user.errors.full_messages.empty?.!  # my solution
+  end
+
+# Test valid signup
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do 
+      post users_path, params: { user: { name: "name valid",
+                                         email: "user@valid.com",
+                                         password: "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_equal 'Welcome to our community!', flash[:success]
+  end
+  
+
+end
