b835b94431d2faca136deb4ecdd31bc3bd4297d3
Ubuntu <ubuntu@ip-172-31-45-115.ec2.internal>
2020-10-26 02:37:31 +0000

    Add MailGun Heroku Addon config files to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,19 +65,19 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   
-  #config.action_mailer.raise_delivery_errors = true
-  #config.action_mailer.delivery_method = :smtp
-  #host = '<your heroku app>.herokuapp.com'
-  #config.action_mailer.default_url_options = { host: host }
-  #ActionMailer::Base.smtp_settings = {
-  #  :address        => 'smtp.sendgrid.net',
-  #  :port           => '587',
-  #  :authentication => :plain,
-  #  :user_name      => ENV['SENDGRID_USERNAME'],
-  #  :password       => ENV['SENDGRID_PASSWORD'],
-  #  :domain         => 'heroku.com',
-  #  :enable_starttls_auto => true
-  #}
+  # Heroku-MailGun Addon send with SMTP via Action Mailer
+  config.action_mailer.raise_delivery_errors = true
+
+  ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'ancient-caverns-81113.heroku.com',
+  :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
   
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
