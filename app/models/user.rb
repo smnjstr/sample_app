65872403873d92cc19e50628fec933133484c22d
Ubuntu <ubuntu@ip-172-31-45-115.ec2.internal>
2020-10-26 01:21:15 +0000

    Fix double dot e-mail check

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
   # Old VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  # Regex to catche double dot
+  # Regex to catch double dot
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
