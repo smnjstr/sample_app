3ac8c21a1c99677da4f3902c54a4fc72e17bbe0b
Ubuntu <ubuntu@ip-172-31-45-115.ec2.internal>
2021-01-11 19:34:45 +0000

    Add user following and feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,20 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
-  # Old VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   # Regex to catch double dot
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 255 },
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
   has_secure_password
@@ -52,39 +59,61 @@ class User < ApplicationRecord
 
   # Activates an account.
   def activate
-    update_columns(activated: true, activated_at: Time.zone.now)
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
   end
 
   # Sends activation email.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
-  
+
   # Sets the password reset attributes.
   def create_reset_digest
     self.reset_token = User.new_token
     update_attribute(:reset_digest,  User.digest(reset_token))
     update_attribute(:reset_sent_at, Time.zone.now)
   end
-  
-  # Sends password reset email
+
+  # Sends password reset email.
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
   end
-  
+
   # Returns true if a password reset has expired.
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
-  
-  # Defines a proto-feed.
-  # See "Following users" for the full implementation.
+
+    # Returns a user's status feed.
   def feed
-    Micropost.where("user_id = ?", id)
+    part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
+    Micropost.left_outer_joins(user: :followers)
+             .where(part_of_feed, { id: id }).distinct
+#   Pre-Exercise version of status feed
+#    following_ids = "SELECT followed_id FROM relationships
+#                     WHERE  follower_id = :user_id"
+#    Micropost.where("user_id IN (#{following_ids})
+#                     OR user_id = :user_id", user_id: id)
+    
+  end
+
+  # Follows a user.
+  def follow(other_user)
+    following << other_user unless self == other_user
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
-
 
     # Converts email to all lower-case.
     def downcase_email
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -45,7 +45,7 @@ class UserTest < ActiveSupport::TestCase
                            foo@bar_baz.com foo@bar+baz.com foo@bar..com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
-      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+      assert !@user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
 
@@ -76,4 +76,43 @@ class UserTest < ActiveSupport::TestCase
       @user.destroy
     end
   end  
+  
+  test "should follow and unfollow a user" do
+    alexander = users(:alexander)
+    betty = users(:betty)
+    assert !alexander.following?(betty)
+    alexander.follow(betty)
+    assert alexander.following?(betty)
+    assert betty.followers.include?(alexander)
+    alexander.unfollow(betty)
+    assert !alexander.following?(betty)
+    alexander.follow(alexander)
+    assert !alexander.following?(alexander)
+  end
+  
+    test "feed should have the right posts" do
+    alexander = users(:alexander)
+    betty  = users(:betty)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert alexander.feed.include?(post_following)
+    end
+    # Self-posts for user with followers
+    alexander.microposts.each do |post_self|
+      assert alexander.feed.include?(post_self)
+    end
+    # Self-posts for user with no followers
+    betty.microposts.each do |post_self|
+      assert betty.feed.include?(post_self)
+      assert_equal alexander.feed.distinct, alexander.feed
+    end
+    # Posts from unfollowed user
+    betty.microposts.each do |post_unfollowed|
+      assert_not alexander.feed.include?(post_unfollowed)
+    end
+  end
+
+  
+  
 end
